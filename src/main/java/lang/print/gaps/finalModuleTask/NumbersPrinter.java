583e0ce7b01080027ef732d7f47da1214d749cc2
Denis Lee <51025257+isleeden@users.noreply.github.com>
2022-08-06 14:16:06 +0500

    Update NumbersPrinter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,6 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-
+        String[] result = {"true", "false"};
+        System.out.println(result[(number >> 31) & 1]);
     }
 }
