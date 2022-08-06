cbb0793759f9dc97565cbc716c1804d72a4d67fc
Denis Lee <51025257+isleeden@users.noreply.github.com>
2022-08-06 13:59:03 +0500

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
-    public void calculateSum(int number) {
+    public void calculateSum(int number) {  
+        int number1 = number / 1000;
+        int number2 = number % 1000 / 100;
+        int number3 = number % 1000 % 100 / 10;
+        int number4 = number % 1000 % 100 % 10;
+        System.out.println(number1 + number2 + number3 +number4);
     }
 }
